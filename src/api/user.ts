a259dc73ad294771d4dd548e245f7c9897adb1cf
coitpark <minhobag18@gmail.com>
2026-06-11 16:16:54 +0900

    feat : set url

--- src/api/user.ts
+++ src/api/user.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 // const API_BASE_URL = "http://localhost:8000";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
